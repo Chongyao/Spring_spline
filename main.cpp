96773b2bc6a7f93eb95869debaa31d5bdd8a6c92
Chongyao <zcyqqxq@vip.qq.com>
2017-10-08 12:58:18 +0800

    finally done shortest version

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include"halfedge.h"
 #include"simplifymesh.h"
-#define iter_times 36
+#define iter_times 300
 using namespace std;
 
 int main()
 {
     halfedge data;
-    string Input="../data/input/dog.obj";
+    string Input="../data/input/sphere.obj";
     data.ReadData(Input);
     data.ConstructHalfedge();
     simplify_mesh data_simp(data);
     data_simp. Simp_shorstest(iter_times);
-    string Output = "../data/output/dog_out.obj";
+    string Output = "../data/output/sphere_out.obj";
     data_simp.mesh_init. halfedge_to_obj(Output);
     return 0;
 }
